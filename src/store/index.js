aa7101e3ba871569b9f6cea738707828b9414a3f
ola marszalek <ola.0608@gmail.com>
2023-03-24 16:58:47 +0100

    added files

---
+++ src/store/index.js
@@ -0,0 +1,28 @@
+import { createSlice, configureStore } from "@reduxjs/toolkit";
+
+const initialState = { counter: 0, showCounter: true };
+
+const counterSlice = createSlice({
+  name: "counter",
+  initialState: initialState,
+  reducers: {
+    increment(state) {
+      state.counter++;
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    increase(state, action) {
+      state.counter = state.counter + action.amount;
+    },
+    toggleCounter(state) {
+        state.showCounter = !state.showCounter;
+    },
+  },
+});
+
+const store = configureStore({
+    reducer: { counter: counterSlice.reducer}
+});
+
+export default store;
